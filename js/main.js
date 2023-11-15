9c8d5dbde72c52232d1f61f5086740acf3202221
harish.nadikatla <harishnadikatla24@gmail.com>
2023-11-15 11:45:02 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+console.log("Hello world");
+
+const gh = "45";
+
+const bh = 54.54;
+
+console.log(Number(gh) + 5566);
+console.log(typeof(Number(gh)));
+
+console.log(Math.floor(Math.random() * 10 + 4781));
+console.log(Math.floor(Math.random() * 10 + 1));
+console.log("HARISHNADIKATLa".charAt(Math.floor(Math.random() * 10 + 1)));
+
+
+
+
+
+
